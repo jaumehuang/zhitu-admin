f7c1221c9e44a1f41fb4c803091a669a0d92288d
jaumehaung <1332204564@qq.com>
2018-06-22 14:58:28 +0800

    抖音

---
+++ src/components/common/page2.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        {{title}}
+    </div>
+</template>
+<script>
+export default {
+    
+    data(){
+         
+         return{
+            title:"我只是皮卡丘2号"
+         }
+    },
+    created:function(){
+
+        //  let locator = new ActiveXObject ("WbemScripting.SWbemLocator");
+        //  let service = locator.ConnectServer(".");
+        //  let properties = service.ExecQuery("SELECT * FROM Win32_BaseBoard");
+        
+    }
+
+}
+</script>
+
